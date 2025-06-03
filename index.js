7fa8f34ee37b42930fd4ea7087ed6b5532bc753e
Suryateja2006 <suryateja2neti@gmail.com>
2025-06-03 16:54:51 +0530

    port

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ mongoose.connection.once("open", () => {
 });
 
 
-const PORT = 8080;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
